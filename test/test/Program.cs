f5d6df797a0a59a7f00e08dbf2bd7ec8c1fcb837
niezl <nzlwan@126.com>
2016-03-08 16:55:11 +0800

    dns.hostname

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,8 @@ namespace test
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Dns.GetHostName());
+            Console.Read();
         }
     }
 }
