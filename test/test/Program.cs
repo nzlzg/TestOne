6b7f227c586bccfb234884bc0fba5f0b3cf535ef
niezl <nzlwan@126.com>
2016-03-10 17:18:44 +0800

    2016-03-10 递归查询XML文件指定属性

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace test
 {
@@ -11,8 +13,26 @@ namespace test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Dns.GetHostName());
+            string xmlstring = "<?xml version='1.0' encoding='utf - 8'?> <FileSystem><DriverC><Dir Dirname = 'MSQCOS22'><File FileName = 'Commond.tt'>"
+                +"</File><Dir DirName = 'MSQD58'><File FileName = 'Request.oci'></File></Dir></Dir><File FileName = '_desktop.ini'></File>"
+       +" <File FileName = 'io.sys'></File></DriverC></FileSystem> ";
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(xmlstring);
+            GetXmlPropertyValue(xmlDoc.ChildNodes);
             Console.Read();
+        }
+      static  void GetXmlPropertyValue(XmlNodeList nodeList)
+        {
+            foreach (XmlNode node in nodeList)
+            {
+                if (node.Name == "File")
+                {
+                    Console.WriteLine(node.Attributes["FileName"].Value);
+                }
+                else if(node.ChildNodes.Count>0){
+                    GetXmlPropertyValue(node.ChildNodes);
+                }
+            }
         }
     }
 }
