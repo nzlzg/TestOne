50a963f0f58656669b3dfb53374afd65760dc1b0
niezl <nzlwan@126.com>
2016-03-17 11:15:29 +0800

    redis operate

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -6,6 +6,8 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using ServiceStack.Redis;
+using ServiceStack.Redis.Support;
 
 namespace test
 {
@@ -13,14 +15,21 @@ namespace test
     {
         static void Main(string[] args)
         {
+            // GetXmlInitialization() //遍历xml节点(递归)
+            simpleRedisGetSet();
+            Console.Read();
+        }
+        #region GetXmlInitialization()
+
+        static void GetXmlInitialization() {
             string xmlstring = "<?xml version='1.0' encoding='utf - 8'?> <FileSystem><DriverC><Dir Dirname = 'MSQCOS22'><File FileName = 'Commond.tt'>"
-                +"</File><Dir DirName = 'MSQD58'><File FileName = 'Request.oci'></File></Dir></Dir><File FileName = '_desktop.ini'></File>"
-       +" <File FileName = 'io.sys'></File></DriverC></FileSystem> ";
+              + "</File><Dir DirName = 'MSQD58'><File FileName = 'Request.oci'></File></Dir></Dir><File FileName = '_desktop.ini'></File>"
+     + " <File FileName = 'io.sys'></File></DriverC></FileSystem> ";
             XmlDocument xmlDoc = new XmlDocument();
             xmlDoc.LoadXml(xmlstring);
             GetXmlPropertyValue(xmlDoc.ChildNodes);
-            Console.Read();
         }
+
       static  void GetXmlPropertyValue(XmlNodeList nodeList)
         {
             foreach (XmlNode node in nodeList)
@@ -34,5 +43,42 @@ namespace test
                 }
             }
         }
+
+        #endregion
+
+        #region simpleRedisGetSet()
+
+        static void simpleRedisGetSet() {
+            string host = "localhost";
+            string elementKey = "testKeyRedis";
+            RedisClient redisclient = new RedisClient(host);
+            redisclient.Remove(elementKey);
+            var storeMembers = new List<string> { "asd","dsd","bb"};
+            // storeMembers.ForEach(x=>redisclient.AddItemToList(elementKey,x));
+
+            redisclient.AddRangeToList(elementKey,storeMembers);
+            var members = redisclient.GetAllItemsFromList(elementKey);
+            var list = redisclient.Lists[elementKey];
+            list.Clear();
+            var ser = new ObjectSerializer();
+            bool result = redisclient.Set<byte[]>(elementKey, ser.Serialize(storeMembers));
+            if (result)
+            {
+                var resultlist = ser.Deserialize(redisclient.Get<byte[]>(elementKey));
+            }
+            members.ForEach(s=>Console.WriteLine("list:"+s));
+         //   Console.WriteLine("index2:"+redisclient.GetItemFromList(elementKey,1));
+            //if (redisclient.Get<string>(elementKey) == null)
+            //{
+            //    redisclient.Set(elementKey, "defaultValue");
+            //}
+            //else {
+            //    redisclient.Set(elementKey, "new default value");
+            //}
+            //Console.WriteLine("redise:value:"+redisclient.Get<string>(elementKey));
+            Console.WriteLine(redisclient.Info);
+        }
+
+        #endregion
     }
 }
